0190d20ccc2df9d6901e8813c8f1dd0bdef73a35
chikou2045-dotcom <289961644+chikou2045-dotcom@users.noreply.github.com>
2026-07-14 08:11:44 +0800

    Update daily brief 2026-07-14

--- data/archive/index.js
+++ data/archive/index.js
@@ -1,5 +1,10 @@
 window.BRIEF_ARCHIVE = [
   {
+    "date": "2026-07-14",
+    "label": "2026年7月14日",
+    "file": "./data/archive/2026-07-14.js"
+  },
+  {
     "date": "2026-07-13",
     "label": "2026年7月13日",
     "file": "./data/archive/2026-07-13.js"
